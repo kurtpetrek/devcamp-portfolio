38928dbb1febcd7727dd66738c1630be0ee08ae9
Kurt Petrek <kurtpetrek@gmail.com>
2017-05-05 19:21:08 -0400

    implementing drafts for blogs

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -57,6 +57,11 @@ class BlogsController < ApplicationController
     end
   end
 
+  def toggle_status_blog
+    byebug
+    redirect_to blogs_url
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_blog
